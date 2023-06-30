9a8363f0cbcbe58cd027b81ade11702cc2fcd35b
SrTriXz <emmanuel.toresrestrepe.5@gmail.com>
2023-06-30 16:33:11 -0500

    commit obligatorio

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-sdsd
+const clickGmail = document.querySelector(".navbar-email")
+const desktopMenu = document.querySelector(".desktop-menu")
+
+
+clickGmail.addEventListener("click",navbarEmail)
+
+
+alert("hola")
+console.log("hola que pasa cabron")
+
+
+
+function navbarEmail(){
+    desktopMenu.classList.toggle("inactive")
+    
+
+}
